4ca0346c43d3fbf62a9633f490d76c8eac10c70e
u-takasaki <146915455+u-takasaki@users.noreply.github.com>
2023-11-02 15:28:33 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+btn.addEventListener('click', ()=>{
+  text.textContent = 'ボタンをクリックしました';
+});
